f2c86d2eabf7f3fec13939a4a52667a63efe0d67
oscar <oo.ortega2247@gmail.com>
2018-05-31 19:57:06 -0500

    run simulations with pathos pool and scipyodesimulator

--- EARM/run_earm2.py
+++ EARM/run_earm2.py
@@ -1,14 +1,21 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from pysb.simulator import ScipyOdeSimulator
+from pathos.multiprocessing import ProcessingPool as Pool
 from earm2_flat import model
+import pickle
 
 tspan = np.linspace(0, 20000, 100)
 
-sim = ScipyOdeSimulator(model, tspan=tspan).run().all
 
-plt.plot(tspan, sim['cPARP'], label='cPARP')
-plt.xlabel('Time(s)')
-plt.ylabel('Population')
-plt.legend()
-plt.show()
+def run_simulation(param_values):
+    sim = ScipyOdeSimulator(model, tspan=tspan).run(param_values=param_values).species
+    return sim
+
+all_parameters = np.load('sampled_kd_pars.npy')
+cpu_cores = 4
+p = Pool(cpu_cores)
+res = p.amap(run_simulation, all_parameters)
+sims = res.get()
+
+with open('outfile', 'wb') as fp:
+    pickle.dump(sims, fp)
