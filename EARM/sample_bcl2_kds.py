756ae03cac3bbb05fc8521200e5c2a5cc22b875a
oscar <oo.ortega2247@gmail.com>
2018-06-03 15:20:51 -0500

    comment out sampling of parameters

--- EARM/sample_bcl2_kds.py
+++ EARM/sample_bcl2_kds.py
@@ -31,26 +31,27 @@ def sample_kd_uniform(parameter_idx, size):
         raise ValueError('Distribution is not defined for parameter index {}'.format(parameter_idx))
 
 
-parameters = np.array([p.value for p in model.parameters])
+# parameters = np.array([p.value for p in model.parameters])
+#
+# samples = 20000
+# repeated_parameter_values = np.tile(parameters, (samples, 1))
+#
+# for par_idx in kr_pars_to_sample:
+#     repeated_parameter_values[:, par_idx] = sample_kr_uniform(samples)
+#
+# for par_idx in kf_pars_to_calculate:
+#     repeated_parameter_values[:, par_idx] = repeated_parameter_values[:, par_idx+1] / sample_kd_uniform(par_idx+1, samples)
+# np.save('sampled_kd_ic.npy', repeated_parameter_values)
 
-samples = 50000
-repeated_parameter_values = np.tile(parameters, (samples, 1))
-
-for par_idx in kr_pars_to_sample:
-    repeated_parameter_values[:, par_idx] = sample_kr_uniform(samples)
-
-for par_idx in kf_pars_to_calculate:
-    repeated_parameter_values[:, par_idx] = repeated_parameter_values[:, par_idx+1] / sample_kd_uniform(par_idx+1, samples)
-
-tspan = np.linspace(0, 20000, 100)
-
-vol= 1e-19
-integrator_opt = {'rtol': 1e-6, 'atol': 1e-6, 'mxsteps': 200000}
-sims = CupSodaSimulator(model, tspan=tspan, gpu=0, memory_usage='shared_constant', vol=vol, obs_species_only=False,
-                        integrator_options=integrator_opt).run(param_values=repeated_parameter_values)
-sims.save('earm_cupsoda_sampled_kd.h5')
-
-signatures = run_tropical_multi(model=model, simulations=sims, cpu_cores=30, verbose=True)
-
-with open('earm_signatures_sampled_kd.pickle', 'wb') as handle:
-    pickle.dump(signatures, handle, protocol=pickle.HIGHEST_PROTOCOL)
+# tspan = np.linspace(0, 20000, 100)
+#
+# vol= 1e-19
+# integrator_opt = {'rtol': 1e-6, 'atol': 1e-6, 'mxsteps': 20000}
+# sims = CupSodaSimulator(model, tspan=tspan, gpu=0, memory_usage='shared_constant', vol=vol,
+#                         integrator_options=integrator_opt).run(param_values=repeated_parameter_values)
+# sims.save('earm_cupsoda_sampled_kd.h5')
+#
+# signatures = run_tropical_multi(model=model, simulations=sims, cpu_cores=30, verbose=True)
+#
+# with open('earm_signatures_sampled_kd.pickle', 'wb') as handle:
+#     pickle.dump(signatures, handle, protocol=pickle.HIGHEST_PROTOCOL)
