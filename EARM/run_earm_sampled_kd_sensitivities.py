8eae8ca09c58c55cedd21e7203787e55d2b43a8b
oscar <oo.ortega2247@gmail.com>
2018-06-06 19:19:35 -0500

    updated path to save simulations

--- EARM/run_earm_sampled_kd_sensitivities.py
+++ EARM/run_earm_sampled_kd_sensitivities.py
@@ -26,16 +26,16 @@ def sims_kd(label):
 
         pars_label1 = pars[np.where(clus_sp37_labels == label)]
         # pars_label1[:, 63] = pars_ref1[63] * 0.8  # 20% Knock down of bax
-        pars_label1[:, 58] = pars_ref1[58] * 0.8  # 20% Knock down of bcl2
+        pars_label1[:, 58] = pars_ref1[58] * 0.2  # 20% Knock down of bcl2
         # pars_label1[:, 56] = pars_ref[56] * 0.8  # 20% knocl down of bclxl
         sim1 = ScipyOdeSimulator(model, tspan=tspan, param_values=pars_label1).run()
-        sim1.save('earm_scipyode_sims_good{0}.h5'.format(label))
+        sim1.save('sims_kd80_sensitivities_sampled_kd/earm_scipyode_sims_good{0}.h5'.format(label))
 
         pars_label2 = pars[np.where(clus_sp37_labels == label)]
         # pars_label2[:, 63] = pars_ref2[63] * 0.8  # 20% Knock down of bax
-        pars_label2[:, 57] = pars_ref2[57] * 0.8  # 20% Knock down of mcl1
+        pars_label2[:, 57] = pars_ref2[57] * 0.2  # 20% Knock down of mcl1
         sim2 = ScipyOdeSimulator(model, tspan=tspan, param_values=pars_label2).run()
-        sim2.save('earm_scipyode_sims_bad{0}.h5'.format(label))
+        sim2.save('sims_kd80_sensitivities_sampled_kd/earm_scipyode_sims_bad{0}.h5'.format(label))
 
     if label == 1:
         """
@@ -46,16 +46,16 @@ def sims_kd(label):
         pars_label1 = pars[np.where(clus_sp37_labels == label)]
         # pars_label1[:, 63] = pars_ref1[63] * 0.8  # 20% Knock down of bax
         # pars_label1[:, 64] = pars_label1[64] * 0.8  # 20% Knock down of bak
-        pars_label1[:, 56] = pars_ref1[56] * 0.8  # 20% Knock down of bclxl
+        pars_label1[:, 56] = pars_ref1[56] * 0.2  # 20% Knock down of bclxl
         sim1 = ScipyOdeSimulator(model, tspan=tspan, param_values=pars_label1).run()
-        sim1.save('earm_scipyode_sims_good{0}.h5'.format(label))
+        sim1.save('sims_kd80_sensitivities_sampled_kd/earm_scipyode_sims_good{0}.h5'.format(label))
 
         pars_label2 = pars[np.where(clus_sp37_labels == label)]
         # pars_label2[:, 63] = pars_ref2[63] * 0.8  # 20% Knock down of bax
         # pars_label2[:, 64] = pars_label2[64] * 0.8  # 20% Knock down of bak
-        pars_label2[:, 58] = pars_ref2[58] * 0.8  # 20% Knock down of bcl2
+        pars_label2[:, 58] = pars_ref2[58] * 0.2  # 20% Knock down of bcl2
         sim2 = ScipyOdeSimulator(model, tspan=tspan, param_values=pars_label2).run()
-        sim2.save('earm_scipyode_sims_bad{0}.h5'.format(label))
+        sim2.save('sims_kd80_sensitivities_sampled_kd/earm_scipyode_sims_bad{0}.h5'.format(label))
 
     if label == 2:
         """
@@ -65,15 +65,15 @@ def sims_kd(label):
         """
         pars_label1 = pars[np.where(clus_sp37_labels == label)]
         # pars_label1[:, 63] = pars_ref1[63] * 0.8  # 20% Knock down of bax
-        pars_label1[:, 57] = pars_ref1[57] * 0.8  # 20% Knock down of mcl1
+        pars_label1[:, 57] = pars_ref1[57] * 0.2  # 20% Knock down of mcl1
         sim1 = ScipyOdeSimulator(model, tspan=tspan, param_values=pars_label1).run()
-        sim1.save('earm_scipyode_sims_good{0}.h5'.format(label))
+        sim1.save('sims_kd80_sensitivities_sampled_kd/earm_scipyode_sims_good{0}.h5'.format(label))
 
         pars_label2 = pars[np.where(clus_sp37_labels == label)]
         # pars_label2[:, 63] = pars_ref2[63] * 0.8  # 20% Knock down of bax
-        pars_label2[:, 56] = pars_ref2[56] * 0.8  # 20% Knock down of bclxl
+        pars_label2[:, 56] = pars_ref2[56] * 0.2  # 20% Knock down of bclxl
         sim2 = ScipyOdeSimulator(model, tspan=tspan, param_values=pars_label2).run()
-        sim2.save('earm_scipyode_sims_bad{0}.h5'.format(label))
+        sim2.save('sims_kd80_sensitivities_sampled_kd/earm_scipyode_sims_bad{0}.h5'.format(label))
 
     if label == 3:
         """
@@ -83,15 +83,15 @@ def sims_kd(label):
         """
         pars_label1 = pars[np.where(clus_sp37_labels == label)]
         # pars_label1[:, 63] = pars_ref1[63] * 0.8  # 20% Knock down of bax
-        pars_label1[:, 57] = pars_ref1[57] * 0.8  # 20% Knock down of mcl1
+        pars_label1[:, 57] = pars_ref1[57] * 0.2  # 20% Knock down of mcl1
         sim1 = ScipyOdeSimulator(model, tspan=tspan, param_values=pars_label1).run()
-        sim1.save('earm_scipyode_sims_good{0}.h5'.format(label))
+        sim1.save('sims_kd80_sensitivities_sampled_kd/earm_scipyode_sims_good{0}.h5'.format(label))
 
         pars_label2 = pars[np.where(clus_sp37_labels == label)]
         # pars_label2[:, 63] = pars_ref2[63] * 0.8  # 20% Knock down of bax
-        pars_label2[:, 58] = pars_ref2[58] * 0.8  # 20% Knock down of bcl2
+        pars_label2[:, 58] = pars_ref2[58] * 0.2  # 20% Knock down of bcl2
         sim2 = ScipyOdeSimulator(model, tspan=tspan, param_values=pars_label2).run()
-        sim2.save('earm_scipyode_sims_bad{0}.h5'.format(label))
+        sim2.save('sims_kd80_sensitivities_sampled_kd/earm_scipyode_sims_bad{0}.h5'.format(label))
 
     if label == 4:
         """
@@ -102,12 +102,12 @@ def sims_kd(label):
         pars_label1 = pars[np.where(clus_sp37_labels == label)]
         pars_label1[:, 63] = pars_ref1[63] * 1.2  # 20% overexpression of bax
         sim1 = ScipyOdeSimulator(model, tspan=tspan, param_values=pars_label1).run()
-        sim1.save('earm_scipyode_sims_good{0}.h5'.format(label))
+        sim1.save('sims_kd80_sensitivities_sampled_kd/earm_scipyode_sims_good{0}.h5'.format(label))
 
         pars_label2 = pars[np.where(clus_sp37_labels == label)]
-        pars_label2[:, 63] = pars_ref2[63] * 0.8  # 20% Knock down of bax
+        pars_label2[:, 63] = pars_ref2[63] * 0.2  # 20% Knock down of bax
         sim2 = ScipyOdeSimulator(model, tspan=tspan, param_values=pars_label2).run()
-        sim2.save('earm_scipyode_sims_bad{0}.h5'.format(label))
+        sim2.save('sims_kd80_sensitivities_sampled_kd/earm_scipyode_sims_bad{0}.h5'.format(label))
 
     if label == 5:
         """
@@ -117,15 +117,15 @@ def sims_kd(label):
         """
         pars_label1 = pars[np.where(clus_sp37_labels == label)]
         # pars_label1[:, 63] = pars_ref1[63] * 0.8  # 20% Knock down of Bax
-        pars_label1[:, 56] = pars_ref1[56] * 0.8  # 20% Knock down of BclxL
+        pars_label1[:, 56] = pars_ref1[56] * 0.2  # 20% Knock down of BclxL
         sim1 = ScipyOdeSimulator(model, tspan=tspan, param_values=pars_label1).run()
-        sim1.save('earm_scipyode_sims_good{0}.h5'.format(label))
+        sim1.save('sims_kd80_sensitivities_sampled_kd/earm_scipyode_sims_good{0}.h5'.format(label))
 
         pars_label2 = pars[np.where(clus_sp37_labels == label)]
         # pars_label2[:, 63] = pars_ref2[63] * 0.8  # 20% Knock down of Bax
-        pars_label2[:, 58] = pars_ref2[58] * 0.8  # 20% Knock down of bcl2
+        pars_label2[:, 58] = pars_ref2[58] * 0.2  # 20% Knock down of bcl2
         sim2 = ScipyOdeSimulator(model, tspan=tspan, param_values=pars_label2).run()
-        sim2.save('earm_scipyode_sims_bad{0}.h5'.format(label))
+        sim2.save('sims_kd80_sensitivities_sampled_kd/earm_scipyode_sims_bad{0}.h5'.format(label))
 
     if label == 6:
         """
@@ -135,20 +135,20 @@ def sims_kd(label):
         """
         pars_label1 = pars[np.where(clus_sp37_labels == label)]
         # pars_label1[:, 63] = pars_ref1[63] * 0.8  # 20% Knock down of bax
-        pars_label1[:, 58] = pars_ref1[58] * 0.8  # 20% Knock down of Bcl2
+        pars_label1[:, 58] = pars_ref1[58] * 0.2  # 20% Knock down of Bcl2
 
         # pars_label1[:, 64] = pars_label1[64] * 0.8  # 80% Knock down of bak
         # pars_label1[:, 57] = pars_label1[57] * 0.8  # 80% Knock down of mcl1
         sim1 = ScipyOdeSimulator(model, tspan=tspan, param_values=pars_label1).run()
-        sim1.save('earm_scipyode_sims_good{0}.h5'.format(label))
+        sim1.save('sims_kd80_sensitivities_sampled_kd/earm_scipyode_sims_good{0}.h5'.format(label))
 
         pars_label2 = pars[np.where(clus_sp37_labels == label)]
         # pars_label2[:, 63] = pars_ref2[63] * 0.8 # 80% Knock down of bax
         # pars_label2[:, 64] = pars_label2[64] * 0.8  # 80% Knock down of bak
         # pars_label2[:, 58] = pars_label2[58] * 0.8  # 80% Knock down of bcl2
-        pars_label2[:, 56] = pars_ref2[56] * 0.8  # 80% Knock down of bclxl
+        pars_label2[:, 56] = pars_ref2[56] * 0.2  # 80% Knock down of bclxl
         sim2 = ScipyOdeSimulator(model, tspan=tspan, param_values=pars_label2).run()
-        sim2.save('earm_scipyode_sims_bad{0}.h5'.format(label))
+        sim2.save('sims_kd80_sensitivities_sampled_kd/earm_scipyode_sims_bad{0}.h5'.format(label))
 
     return
 
